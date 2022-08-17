a814aa7be482ce9ee90072a0634705d470ee278a
edisoncardenas151999 <108204103+edisoncardenas151999@users.noreply.github.com>
2022-08-17 12:00:55 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,103 @@
+
+let character = document.getElementById('character')
+let block = document.getElementById('block')
+let counter = 0
+
+
+const backgroundAudio = new Audio();
+backgroundAudio.src = 'http://docs.google.com/uc?export=open&id=12HIvC0ZOlUdVEk9TsvVbJit3xY3JRisE'
+const jumpSound = new Audio();
+jumpSound.src ='http://docs.google.com/uc?export=open&id=1ANefDl3zStoPsOk_8fb7hTL0FcSnNVay'
+
+
+
+document.getElementById("splash").addEventListener("click", remove)
+if (document.getElementById('splash')) {
+ block.style.animation = 'none'
+}
+
+function remove(){
+ let splash = document.getElementById('splash')
+ splash.remove()
+ block.style.animation = 'block 1.5s infinite'
+ start()
+backgroundAudio.play();
+}
+
+function start (){
+  
+function jumpCharacter(){
+  if(character.classList != 'animate'){
+    character.classList.add('animate');
+  }
+    setTimeout(function(){
+        character.classList.remove('animate');
+    },500)
+
+}
+function control(e){
+  if(e.keyCode === 32){
+    jumpSound.play();
+    jumpCharacter();
+    
+    
+    
+  }
+
+}
+
+
+
+ document.addEventListener('keydown', control)
+let detectCollision = setInterval(function(){
+    let characterTop = parseInt(window.getComputedStyle(character).getPropertyValue('top'));
+    let blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue('left'));
+    if(blockLeft < 30 && blockLeft > 0 && characterTop >= 350){
+        block.style.animation = 'none'
+        function reloadPage (){
+        if(alert(`game over your score is ${Math.floor(counter/100)}`)){
+          console.log('alert')
+        }
+        else {
+          window.location.reload()
+        }
+        }
+        reloadPage()
+        counter = 0
+    }
+    else {
+        counter++
+         let gameSpeed = document.getElementById('score')
+         gameSpeed.innerHTML = Math.floor(counter/100) 
+        
+    }
+
+},10);
+   
+  
+}
+
+// if(gameSpeed === 5){
+//   block.style.animation = 'block .3s Infinite'
+// }
+
+
+//TODO: MAKE A IF STATEMENT THAT INCREASES THE SPEED OF THE BLOCK EVERY TIME THE SCORE IS DOUBLED
+//TODO: CHANGE THE IMGS INTO ACTUAL URL LINKS
+//TODO: PUT SOUND EFFECTS AND BACKGROUND MUSIC
+
+
+
+  
+// if (counter === 5){
+//   block.style.animation = 'block 1s infinite '
+// }
+// else if (counter === 10){
+//   block.style.animation = 'block .5s infinite '
+// }
+// else if(counter === 20){
+//   block.style.animation = 'block .3s infinite '
+
+// }
+
+
