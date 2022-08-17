612cf4b2e215f8ab233ade6ebcb4e2a1f7d1487d
edison cardenas <edisoncardenas151999@yahooo.com>
2022-08-17 13:26:07 -0400

    minor changes

--- main.js
+++ main.js
@@ -1,93 +1,75 @@
-
-let character = document.getElementById('character')
-let block = document.getElementById('block')
-let counter = 0
-
+let character = document.getElementById("character");
+let block = document.getElementById("block");
+let counter = 0;
 
 const backgroundAudio = new Audio();
-backgroundAudio.src = 'http://docs.google.com/uc?export=open&id=12HIvC0ZOlUdVEk9TsvVbJit3xY3JRisE'
+backgroundAudio.src =
+  "http://docs.google.com/uc?export=open&id=12HIvC0ZOlUdVEk9TsvVbJit3xY3JRisE";
 const jumpSound = new Audio();
-jumpSound.src ='http://docs.google.com/uc?export=open&id=1ANefDl3zStoPsOk_8fb7hTL0FcSnNVay'
+jumpSound.src =
+  "http://docs.google.com/uc?export=open&id=1ANefDl3zStoPsOk_8fb7hTL0FcSnNVay";
 
-
-
-document.getElementById("splash").addEventListener("click", remove)
-if (document.getElementById('splash')) {
- block.style.animation = 'none'
+document.getElementById("splash").addEventListener("click", remove);
+if (document.getElementById("splash")) {
+  block.style.animation = "none";
 }
 
-function remove(){
- let splash = document.getElementById('splash')
- splash.remove()
- block.style.animation = 'block 1.5s infinite'
- block.style.animationTimingFunction = 'linear'
- start()
-backgroundAudio.play();
+function removeSplash() {
+  let splash = document.getElementById("splash");
+  splash.removeSplash();
+  block.style.animation = "block 1.5s infinite";
+  block.style.animationTimingFunction = "linear";
+  startGame();
+  backgroundAudio.play();
 }
 
-function start (){
-  
-function jumpCharacter(){
-  if(character.classList != 'animate'){
-    character.classList.add('animate');
+function startGame() {
+  function jumpCharacter() {
+    if (character.classList != "animate") {
+      character.classList.add("animate");
+    }
+    setTimeout(function () {
+      character.classList.remove("animate");
+    }, 500);
   }
-    setTimeout(function(){
-        character.classList.remove('animate');
-    },500)
-
-}
-function control(e){
-  if(e.keyCode === 32){
-    jumpSound.play();
-    jumpCharacter();
-    
-    
-    
+  function control(e) {
+    if (e.keyCode === 32) {
+      jumpSound.play();
+      jumpCharacter();
+    }
   }
 
-}
-
-
-
- document.addEventListener('keydown', control)
-let detectCollision = setInterval(function(){
-    let characterTop = parseInt(window.getComputedStyle(character).getPropertyValue('top'));
-    let blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue('left'));
-    if(blockLeft < 30 && blockLeft > 0 && characterTop >= 350){
-        block.style.animation = 'none'
-        function reloadPage (){
-        if(alert(`game over your score is ${Math.floor(counter/100)}`)){
-          console.log('alert')
+  document.addEventListener("keydown", control);
+  let detectCollision = setInterval(function () {
+    let characterTop = parseInt(
+      window.getComputedStyle(character).getPropertyValue("top")
+    );
+    let blockLeft = parseInt(
+      window.getComputedStyle(block).getPropertyValue("left")
+    );
+    if (blockLeft < 30 && blockLeft > 0 && characterTop >= 350) {
+      block.style.animation = "none";
+      function reloadPage() {
+        if (alert(`game over your score is ${Math.floor(counter / 100)}`)) {
+          console.log("alert");
+        } else {
+          window.location.reload();
         }
-        else {
-          window.location.reload()
-        }
-        }
-        reloadPage()
-        counter = 0
+      }
+      reloadPage();
+      counter = 0;
+    } else {
+      counter++;
+      let gameSpeed = document.getElementById("score");
+      gameSpeed.innerHTML = Math.floor(counter / 100);
     }
-    else {
-        counter++
-         let gameSpeed = document.getElementById('score')
-         gameSpeed.innerHTML = Math.floor(counter/100) 
-        
-    }
-
-},10);
-   
-  
+  }, 10);
 }
-
-
-
 
 //TODO: MAKE A IF STATEMENT THAT INCREASES THE SPEED OF THE BLOCK EVERY TIME THE SCORE IS DOUBLED
 //TODO: CHANGE THE IMGS INTO ACTUAL URL LINKS
 //TODO: PUT SOUND EFFECTS AND BACKGROUND MUSIC
 
-
-
-  
 // if (counter.innerHTML === 5){
 //   block.style.animation = 'block 1s infinite '
 // }
@@ -98,5 +80,3 @@ let detectCollision = setInterval(function(){
 //   block.style.animation = 'block .3s infinite '
 
 // }
-
-
